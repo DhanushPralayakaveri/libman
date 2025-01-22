0831607e584dbd8e27ac79d41378fdaba3c083a4
Dhanush <dhanushpralayakaveri@gmail.com>
2025-01-22 12:44:35 +0530

    file got

---
+++ src/com/mtechdemo/bean/User.java
@@ -0,0 +1,20 @@
+package com.mtechdemo.bean;
+
+//import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+public class User {
+private int id;
+private String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
